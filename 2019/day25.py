395d25096471b748e442f1392fd7a7744be0f574
Dimitris Karakostas <dimit.karakostas@gmail.com>
2019-12-25 18:54:10 +0200

    [2019] Day 25, part 1

---
+++ 2019/day25.py
@@ -0,0 +1,131 @@
+from day25_moves import logged_moves
+from itertools import combinations
+
+class Program:
+    def __init__(self, memory):
+        self.memory = memory
+        self.halted, self.outputs, self.idx, self.rel_base = False, [], 0, 0
+
+    def run(self, inputs):
+        self.outputs = []
+        used_inputs = 0
+        while self.idx < len(self.memory):
+            instruction = str(self.memory[self.idx])
+            opcode, modes = int(instruction[-2:]), instruction[:-2][::-1] + 3 * '0'
+            self.idx += 1
+            if opcode in (1, 2, 5, 6, 7, 8):  # Two params
+                ins = []
+                for i in range(2):
+                    param = self.memory[self.idx + i]
+                    if modes[i] == '0':
+                        if param > len(self.memory) - 1:
+                            self.memory += [0 for _ in range(param)]
+                        ins.append(self.memory[param])
+                    elif modes[i] == '1':
+                        ins.append(param)
+                    elif modes[i] == '2':
+                        if param + self.rel_base > len(self.memory) - 1:
+                            self.memory += [0 for _ in range(param + self.rel_base)]
+                        ins.append(self.memory[param + self.rel_base])
+                if opcode in (1, 2, 7, 8):  # Two params, one output
+                    assert modes[2] in ('0', '2'), 'Invalid write'
+                    out = self.memory[self.idx + 2]
+                    if modes[2] == '2':
+                        out += self.rel_base
+                    if out > len(self.memory) - 1:
+                        self.memory += [0 for _ in range(out)]
+                    if opcode in (1, 2):
+                        self.memory[out] = ins[0] + ins[1] if opcode == 1 else ins[0] * ins[1]
+                    else:
+                        if opcode == 7:
+                            self.memory[out] = 1 if ins[0] < ins[1] else 0
+                        else:
+                            self.memory[out] = 1 if ins[0] == ins[1] else 0
+                    self.idx += 3
+                elif opcode in (5, 6):  # Two params, no output
+                    if any([
+                        opcode == 5 and ins[0] != 0,
+                        opcode == 6 and ins[0] == 0,
+                    ]):
+                        self.idx = ins[1]
+                    else:
+                        self.idx += 2
+            elif opcode in (3, 4, 9):  # Single param
+                param = self.memory[self.idx]
+                if param + self.rel_base > len(self.memory) - 1:
+                    self.memory += [0 for _ in range(param + self.rel_base)]
+                if opcode == 3:
+                    assert modes[0] in ('0', '2'), 'Invalid write'
+                    if used_inputs < len(inputs):
+                        inp = inputs[used_inputs]
+                        used_inputs += 1
+                        if modes[0] == '0':
+                            self.memory[param] = inp
+                        elif modes[0] == '2':
+                            self.memory[param + self.rel_base] = inp
+                    else:
+                        self.idx -= 1
+                        return used_inputs
+                elif opcode == 4:  # Single input, one output
+                    if modes[0] == '0':
+                        out = self.memory[param]
+                    elif modes[0] == '1':
+                        out = param
+                    elif modes[0] == '2':
+                        out = self.memory[param + self.rel_base]
+                    self.outputs.append(out)
+                elif opcode == 9:
+                    if modes[0] == '0':
+                        self.rel_base += self.memory[param]
+                    elif modes[0] == '1':
+                        self.rel_base += param
+                    elif modes[0] == '2':
+                        self.rel_base += self.memory[param + self.rel_base]
+                self.idx += 1
+            elif opcode in (99, ):  # No param
+                self.halted = True
+                break
+            else:
+                assert False, 'Unknown opcode'
+        assert self.halted, 'Exit without halting'
+
+
+program = [int(i) for i in open('input25').readlines()[0].split(',')]
+p = Program(program[:])
+
+# STEP 1: Manually explore the whole space, find all items you can carry, take them to the security check and drop them
+manually_explore = False
+if manually_explore:
+    moves = []
+    while False:
+        print ''.join(chr(i) for i in p.outputs)
+        inp = raw_input()
+        move = [ord(i) for i in inp + '\n']
+        moves.append(move)
+        p.run(move)
+        open('day25_moves.py', 'w').write('logged_moves = ' + str(moves))
+
+# STEP 2: Check all item combinations to find the correct one
+if not manually_explore:
+    items = set([
+        'spool of cat6',
+        'space law space brochure',
+        'asterisk',
+        'jam',
+        'shell',
+        'astronaut ice cream',
+        'space heater',
+        'klein bottle',
+    ])
+    p.run([i for sublist in logged_moves for i in sublist])
+    combs = sum(map(lambda r: list(combinations(items, r)), range(1, len(items) + 1)), [])
+    for comb in combs:
+        print '[*]', ','.join(comb)
+        for item in comb:
+            p.run([ord(i) for i in 'take ' + item + '\n'])
+        p.run([ord(i) for i in 'south\n'])
+        if 'Alert!' not in ''.join(chr(i) for i in p.outputs):
+            print ''.join(chr(i) for i in p.outputs)
+            break
+        for item in comb:
+            p.run([ord(i) for i in 'drop ' + item + '\n'])
